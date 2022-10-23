e5bb18399db1cde570fb3052822b72a5e66f7470
Abhay! <77115967+AbhxyDxs@users.noreply.github.com>
2022-10-23 18:48:37 +0530

    Update prgrm1-circle.py

--- prgrm1-circle.py
+++ prgrm1-circle.py
@@ -1,4 +1,4 @@
-print("Program to find Perimeter and Area of a Circle")
+print("Program to find Perimeter and Area of a Circle \n")
 r=float(input("Enter the Radius of the Circle : "))
 p=2*(3.1415926535898)*r
 a=(3.1415926535898)*r*r
