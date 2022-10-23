c54b32f89c2f8be786fad012e995f141d30f7e33
Abhay! <77115967+AbhxyDxs@users.noreply.github.com>
2022-10-23 19:00:04 +0530

    Add files via upload

---
+++ test/prgrm5.py
@@ -0,0 +1,4 @@
+print("Basic for loop")
+a=int(input("Enter a Number : "))
+for i in range(a):
+	print(i)
---
+++ test/prgrm6.py
@@ -0,0 +1,8 @@
+print("Factorial of a Number")
+a=int(input("Enter a Number : "))
+x=1
+for i in range(1,a+1):
+	x=x*i
+print(a,"! = ",x)
+	
+	
