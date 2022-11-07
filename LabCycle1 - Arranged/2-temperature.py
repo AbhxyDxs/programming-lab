33e835f4ba9aef91ab171f7a821492fe2cb0000b
Abhay! <77115967+AbhxyDxs@users.noreply.github.com>
2022-11-07 12:44:28 +0530

    Delete LabCycle1 - Arranged directory

--- LabCycle1 - Arranged/2-temperature.py
+++
@@ -1,5 +0,0 @@
-print("Program to Covert Temperature in Celcius to Fahrenheit \n")
-c=float(input("Enter Temperature in Celcius : "))
-f=c*(1.8)+32
-print("Given temp in Fahrenheit = ",f)
-
--- LabCycle1 - Arranged/20-even_remove.py
+++
@@ -1,9 +0,0 @@
-a=input("Enter Numbers : ")
-a=a.split(" ")
-b=[]
-for i in a:
-	b.append(int(i))
-c=[x for x in b if x%2!=0]
-print(b)
-print("After Remving Even Numbers : ")
-print(c)
