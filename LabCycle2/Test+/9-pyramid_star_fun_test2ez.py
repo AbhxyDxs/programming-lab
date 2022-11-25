f1d508bfd2f19036d5d2199b88c6564d5bea82b8
Abhay! <77115967+AbhxyDxs@users.noreply.github.com>
2022-11-25 10:57:01 +0530

    Add files via upload

---
+++ LabCycle2/Test+/9-pyramid_star_fun_test2ez.py
@@ -0,0 +1,10 @@
+#without using nested loops!!!!
+
+def star_pattern():
+    for i in range(1, 6):
+        print(i * "* ")
+    for i in reversed(range(1,5)):
+        print(i * "* ")
+            
+
+star_pattern ()
