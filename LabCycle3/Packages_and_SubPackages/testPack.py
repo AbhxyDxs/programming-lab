03f8d50aab50cdb01df60d8ea889110f5900ae0c
Abhay! <77115967+AbhxyDxs@users.noreply.github.com>
2022-12-09 10:31:43 +0530

    Add files via upload

---
+++ LabCycle3/Packages_and_SubPackages/testPack.py
@@ -0,0 +1,32 @@
+from graphics import circle
+from graphics import rectangle
+
+from graphics.threeD_graphics import sphere
+from graphics.threeD_graphics import cuboid
+
+#import cuboid
+#import sphere
+
+
+r=int(input("Enter Radius of Circle : "))
+
+l=int(input("Enter Length of Rectangle : ")) 
+b=int(input("Enter Breadth of Rectangle : "))
+
+p=int(input("Enter Length of Cuboid : "))
+q=int(input("Enter Breadth of Cuboid : "))
+r=int(input("Enter Height of Cuboid : "))
+
+x=int(input("Enter Radius of Sphere : "))
+
+
+circle.cArea(r)
+circle.cPerim(r)
+rectangle.rArea(l,b)
+rectangle.rPerim(l,b)
+
+cuboid.cubArea(p,q,r)
+cuboid.cubPerim(p,q,r)
+sphere.sArea(x)
+sphere.sPerim(x)
+
